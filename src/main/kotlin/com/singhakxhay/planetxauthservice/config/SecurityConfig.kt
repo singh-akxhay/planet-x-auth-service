06cb4eeedb2949c23f9cd8101273b2d37f32e1e4
Akshay Singh <singh.ak2104@gmail.com>
2023-08-12 09:16:19 +0530

    Added security config

---
+++ src/main/kotlin/com/singhakxhay/planetxauthservice/config/SecurityConfig.kt
@@ -0,0 +1,48 @@
+package com.singhakxhay.planetxauthservice.config
+
+import com.singhakxhay.planetxauthservice.exception.AuthExceptionHandler
+import com.singhakxhay.planetxauthservice.security.JwtAuthFilter
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+class SecurityConfig {
+    @Autowired
+    private lateinit var authExceptionHandler: AuthExceptionHandler
+
+    @Autowired
+    private lateinit var jwtAuthFilter: JwtAuthFilter
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        // Disable CORS
+        http.cors { it.disable() }
+
+        // Disable CSRF
+        http.csrf { it.disable() }
+
+        // Change to stateless session management
+        http.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+        // Add exception handler
+        http.exceptionHandling { it.authenticationEntryPoint(authExceptionHandler) }
+
+        // Authorize http requests
+        http.authorizeHttpRequests {
+            it.apply {
+                requestMatchers("/auth/**").permitAll()
+                anyRequest().authenticated()
+            }
+        }
+
+        // Add Jwt authentication filter
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
