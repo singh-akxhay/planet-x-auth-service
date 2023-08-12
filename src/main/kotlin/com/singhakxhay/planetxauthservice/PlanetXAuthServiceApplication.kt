0eb8ca21a3a102856395ccd88317e5d30d1d07f6
Akshay Singh <singh.ak2104@gmail.com>
2023-08-12 08:12:33 +0530

    Init commit

---
+++ src/main/kotlin/com/singhakxhay/planetxauthservice/PlanetXAuthServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.singhakxhay.planetxauthservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanetXAuthServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanetXAuthServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/singhakxhay/planetxauthservice/PlanetXAuthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.singhakxhay.planetxauthservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanetXAuthServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
